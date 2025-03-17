929c30a72ae120850060b865de27d1535aff9c64
César Gualberto <cjgualberto824@gmail.com>
2025-03-17 19:25:20 -0300

    Atualização

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,25 @@ public class Main {
     public static void main(String[] args) {
             String mensagem = JOptionPane.showInputDialog("Entre com uma String");
 
-            JOptionPane.showMessageDialog(null,mensagem);
 
             char[] caracteres = mensagem.toCharArray();
-            JOptionPane.showMessageDialog(null,caracteres);
+            int[] asciiValores = new int[caracteres.length];
+
+            for (int i = 0; i < caracteres.length; i++) {
+                asciiValores[i] = (int) caracteres[i] + 10; // Soma 10 unidades para criptografar
+            }
+
+            // Conversão de volta para caracteres
+            char[] criptografado = new char[asciiValores.length];
+            for (int i = 0; i < asciiValores.length; i++) {
+                criptografado[i] = (char) asciiValores[i];
+            }
+
+            // Construção da String criptografada
+            String mensagemCriptografada = new String(criptografado);
+
+            // Exibição da mensagem criptografada
+            JOptionPane.showMessageDialog(null, "Mensagem criptografada: " + mensagemCriptografada);
 
     }
 }
